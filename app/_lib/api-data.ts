2a3a855f32f2fab4bde5a5a4f66ca0b146710973
SurajNaidu0 <surajangurajnaidu@gmail.com>
2025-01-09 10:45:40 +0530

    making changes

--- app/_lib/api-data.ts
+++ app/_lib/api-data.ts
@@ -5,11 +5,12 @@ interface EtherscanApiResponse {
 }
 
 export async function getWalletBalance(
-  address: string,
+  address: string[],
   chainId: number,
   apiKey: string
 ): Promise<string> {
-  const apiUrl = `https://api.etherscan.io/v2/api?chainid=${chainId}&module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`;
+  const addressParam = address.join(",");
+  const apiUrl = `https://api.etherscan.io/v2/api?chainid=${chainId}&module=account&action=balancemulti&address=${addressParam}&tag=latest&apikey=${apiKey}`;
   const response = await fetch(apiUrl);
 
   // Type assertion to treat the response as EtherscanApiResponse
@@ -36,4 +37,9 @@ export async function getWalletBalanceERC20(
   const data = (await response.json()) as EtherscanApiResponse;
   const balanceInWei: string = data.result;
   return balanceInWei;
+}
+
+export function weiToEthConvertor(weiAmount: string) {
+  const weiInEther = BigInt(weiAmount) / BigInt(10 ** 18);
+  return weiInEther.toString(); // Return as string to avoid precision loss
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { getWalletBalance } from "./_lib/api-data";
 
 export default async function Home() {
-  const address = "0x3f2d27283ad34b2bf7aa9e117c4e6c63922779f2"; // Replace with a real Ethereum address
+  const address = [
+    "0x3f2d27283ad34b2bf7aa9e117c4e6c63922779f2",
+    "0xf8632dbdf4296d5c1bb63a3a3bfcd64e41d0afb4",
+  ]; // Replace with a real Ethereum address
   const chainId = 1; // Mainnet Ethereum chain ID
   const apiKey = "1GZ5K9C5QM76HQCDJXFANUPHD3UFICFDWI"; // Replace with your real Etherscan API key
 
